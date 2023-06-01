eecfece66bef4918382dbd972c7ba67bcf9402cb
iragul.palanisamy <iragul.palanisamy@volantetech.com>
2023-06-01 23:58:38 +0530

    Reverse program on array method

---
+++ Java_Intermediate_programing/src/String_Programs/Reverse_String_character_array.java
@@ -0,0 +1,26 @@
+package String_Programs;
+
+public class Reverse_String_character_array {
+
+    public static void main(String args[])
+    {
+        String main_string="Ragul reverse the string";
+        //initiate the char array
+        char charArray[];
+        charArray=main_string.toCharArray();
+        int a=0;
+        int b=main_string.length()-1;
+        while (a<b)
+        {
+            char temp=charArray[a];
+            charArray[a]=charArray[b];
+            charArray[b]=temp;
+
+            a++;
+            b--;
+        }
+        String Reversed_String=new String(charArray);
+        System.out.println(Reversed_String);
+
+    }
+}
