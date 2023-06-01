cadec5e24b23a5dac6b912abfbe3182beca31523
iragul.palanisamy <iragul.palanisamy@volantetech.com>
2023-06-02 00:07:12 +0530

    package update

--- Java_Intermediate_programing/src/Reverse_String/Reverse_String_Using_Stringbuilder.java
+++ Java_Intermediate_programing/src/Reverse_String/Reverse_String_Using_Stringbuilder.java
@@ -1,7 +1,7 @@
-package String_Programs;
+package Reverse_String;
 
 public class Reverse_String_Using_Stringbuilder {
-    public  static void main(String args[])
+    public  static void main(String[] args)
     {
         String main_string="Ragul reverse the string";
         String Reversed_String;
--- Java_Intermediate_programing/src/Reverse_String/Reverse_String_character_array.java
+++ Java_Intermediate_programing/src/Reverse_String/Reverse_String_character_array.java
@@ -1,13 +1,12 @@
-package String_Programs;
+package Reverse_String;
 
 public class Reverse_String_character_array {
 
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         String main_string="Ragul reverse the string";
         //initiate the char array
-        char charArray[];
-        charArray=main_string.toCharArray();
+        char charArray[]=main_string.toCharArray();
         int a=0;
         int b=main_string.length()-1;
         while (a<b)
